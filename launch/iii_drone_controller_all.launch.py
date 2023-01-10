3a68e14005b21518c4bbed64537b63c0dc6a7168
Frederik Falk Nyboe <ffn@mmmi.sdu.dk>
2023-01-10 08:14:43 +0100

    full launch files

---
+++ launch/iii_drone_controller_all.launch.py
@@ -0,0 +1,20 @@
+from struct import pack
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    share_dir = get_package_share_directory('iii_drone')
+
+    tf = IncludeLaunchDescription(PythonLaunchDescriptionSource(share_dir + "/launch/tf_real.launch.py"))
+    controller = IncludeLaunchDescription(PythonLaunchDescriptionSource(share_dir + "/launch/controller.launch.py"))
+
+    return LaunchDescription([
+        controller,
+        tf
+    ])
